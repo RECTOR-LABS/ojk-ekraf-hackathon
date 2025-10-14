d47ee60dbff8bbb89c92637f1b98bee0ac844442
RECTOR <rheza10@gmail.com>
2025-10-14 17:16:35 +0700

    chore: Remove original frontend from dev branch
    - dev branch now contains only frontend-design-2/ directory
    - This branch deploys to karyachain-v2.rectorspace.com
    - Keeps deployment clean and focused on glassmorphism design

--- frontend/components/ui/index.ts
+++
@@ -1,7 +0,0 @@
-export { Button, type ButtonProps, buttonVariants } from './button';
-export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from './card';
-export { Input, type InputProps } from './Input';
-export { Spinner, type SpinnerProps } from './Spinner';
-export { ErrorDisplay, InlineError } from './ErrorDisplay';
-export { Skeleton, SkeletonCard, SkeletonGrid } from './Skeleton';
-export { Toaster } from './sonner';
